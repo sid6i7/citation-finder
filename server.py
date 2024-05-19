6f737cd4e124180b0d35b9a2f4b64b4bd9ea4d2c
Siddhant Jain <siddhantjain209@gmail.com>
2024-05-19 13:15:05 +0530

    add cors middleware and add request parameter validation to server

--- server.py
+++ server.py
@@ -1,10 +1,25 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from model.model import Model
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost",
+        "http://localhost:3000",
+    ],
+    allow_credentials=True,
+    allow_methods=["GET", "POST"],
+    allow_headers=["*"],
+)
 
-@app.get("/get_citations")
-def get_citations():
-    model = Model()
-    citations = model.get_citations()
-    return citations
+
+@app.post("/api/get_citations/")
+def get_citations(messages: dict):
+    if messages:
+        model = Model(messages['messages']) 
+        citations = model.get_citations()
+        return citations
+    else:
+        return {"error": "no data provided"}
